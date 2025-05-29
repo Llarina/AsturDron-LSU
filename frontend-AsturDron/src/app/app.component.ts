f748df46e576d9b47c302a62cc01e3d537579517
Llarina <llarinasanzuria@gmail.com>
2025-05-29 11:30:06 +0200

    casi listo

--- frontend-AsturDron/src/app/app.component.ts
+++ frontend-AsturDron/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive, Router } from '@angular/router';
 import { AuthService } from './services/auth.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
@@ -30,9 +30,13 @@ import { RegisterComponent } from './components/auth/register/register.component
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(public authService: AuthService) {}
+  constructor(
+    public authService: AuthService,
+    public router: Router
+  ) {}
 
   logout() {
     this.authService.logout();
+    this.router.navigate(['/']);
   }
 }
--- frontend-AsturDron/src/app/app.routes.ts
+++ frontend-AsturDron/src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { NoticiasComponent } from './noticias/noticias.component';
 import { OcioComponent } from './ocio/ocio.component';
 import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
+import { MeteorologiaComponent } from './meteorologia/meteorologia.component';
 import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/noticias', pathMatch: 'full' },
     { path: 'noticias', component: NoticiasComponent },
     { path: 'ocio', component: OcioComponent, canActivate: [AuthGuard] },
+    { path: 'meteorologia', component: MeteorologiaComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent }
 ];
